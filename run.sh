296c6318ed0a3ad509f13a7c88f41bbaf56a780a
libbydistrito <libertygdistrito@yahoo.com>
2025-11-17 18:24:28 -0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/libbygonzalesdistrito/Desktop/BudgetBuddieProject
+/Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home/bin/javac --module-path /Users/libbygonzalesdistrito/Desktop/javafx25/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics MainApp.java && /Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home/bin/java --module-path /Users/libbygonzalesdistrito/Desktop/javafx25/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics MainApp
